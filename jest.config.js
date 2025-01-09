56809a9ef4ed76016882377f1fe3117906f2fda2
Maria Wemeaux <145456229+Mariaescapereality@users.noreply.github.com>
2025-01-09 13:31:49 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
-"jest": {
-  "transformIgnorePatterns": [
-    "/node_modules/(?!@vue|some-other-package)"
-  ]
-}
+module.exports = {
+  preset: '@vue/cli-plugin-unit-jest',
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.jsx$': 'babel-jest',
+    '^.+\\.ts$': 'babel-jest',
+    '^.+\\.tsx$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'json', 'jsx', 'vue', 'ts', 'tsx'],
+  transformIgnorePatterns: ['/node_modules/(?!@babel|@vue)'],
+};
 
