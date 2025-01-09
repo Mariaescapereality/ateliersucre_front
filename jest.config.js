f857600c79937926f845a4418b1d586b4e4c9019
Maria Wemeaux <145456229+Mariaescapereality@users.noreply.github.com>
2025-01-09 12:28:27 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript',
-};
+"jest": {
+  "transformIgnorePatterns": [
+    "/node_modules/(?!@vue|some-other-package)"
+  ]
+}
+
